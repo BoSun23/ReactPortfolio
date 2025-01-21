8448e33a0e754a511097c9213fe6d6882a1588cd
BoSun23 <sunboadrien@gmail.com>
2025-01-20 22:18:48 -0500

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+/**
+ * @copyright 2024 Bo Sun
+ * @license Apache-2.0
+ */
+
+/**
+ * 
+ * Components
+ */
+
+import Header from "./components/Header";
+
+
+
+const App = () => {
+    return (
+        <Header />
+    );
+  };
+  
+  export default App;
+  
