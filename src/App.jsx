e36fbba37df7c750f758c1dd851e101d67bb85fb
BoSun23 <sunboadrien@gmail.com>
2025-01-22 14:32:58 -0500

    About Done

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@
 
 import Header from "./components/Header";
 import Hero from "./components/Hero";
+import About from "./components/About";
 
 
 
@@ -19,6 +20,7 @@ const App = () => {
         <Header />
         <main>
             <Hero />
+            <About />
         </main>
         </>
     );
