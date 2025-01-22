fa8469a7cef01575857a0c4e616e4008c4d53059
BoSun23 <sunboadrien@gmail.com>
2025-01-21 22:31:17 -0500

    End Hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,21 +3,19 @@
  * @license Apache-2.0
  */
 
-/**
- * Node modules
- */
 
 const Hero = () => {
   return (
     <section
     id="home"
-    className=""
+    className="pt-28 lg:pt-36"
     >
-      <div className="">
+      <div className="container lg:grid lg:grid-cols-2
+      items-center lg:gap-10">
         <div>
 
-          <div className="">
-            <figure className="">
+          <div className="flex items-center gap-3">
+            <figure className="img-box w-9 h-9 rounded-lg">
               <img 
               src="/images/avatar-1.jpg"
               width={40}
@@ -26,18 +24,44 @@ const Hero = () => {
               className="img-cover" 
               />
             </figure>
-            <div className="">
-              <span className="">
-                <span className=""></span>
+            <div className="flex items-center gap-1.5
+            text-zinc-400 text-sm tracking-wide">
+              <span className="relative w-2 h-2 rounded-full bg-emerald-400">
+                <span className="absolute inset-0 rounded-full bg-emerald-400 animate-ping"></span>
               </span>
 
+              Available for work
+
             </div>
+            
+          </div>
+          <h2 className="headline-1 max-w-[15ch] sm:max-w-[20ch] lg:max-w-[15ch] mt-5 mb-8 lg:mb-10">
+              Building Scalable Modern Websites for the Future
+            </h2>
+            <div className="flex items-center gap-3">
+                ButtonPrimary 
+
+
+                ButtonOutline 
 
           </div>
 
 
         </div>
+        <div className="hidden lg:block">
+          <figure className="w-full max-w-[4480px] ml-auto
+          bg-gradient-to-t from-sky-400 via-25% via-sky-40 to-65% 
+          rounded-[60px] overflow-hidden">
+            <img 
+            src="/images/hero-banner.png"
+            width={656}
+            height={800} 
+            alt="Portrait" 
+            className="w-full" 
+            />
+          </figure>
 
+        </div>
 
       </div>
 
