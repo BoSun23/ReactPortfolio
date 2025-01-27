ff4506da3b696c6a420f3ebb92ad1595deba0a34
BoSun23 <sunboadrien@gmail.com>
2025-01-27 12:21:10 -0500

    contact mid

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -40,21 +40,21 @@ const Contact = () => {
         id="contact"
         className="section"
     >
-        <div className="container">
-            <div className="">
-                <h2 className="headline-2">
+        <div className="container lg:grid lg:grid-cols-2 lg:items-stretch">
+            <div className="mb-12 lg:mb-0 lg:flex lg:flex-col">
+                <h2 className="headline-2 lg:max-w-[12ch]">
                 Contact me for collaboration
                 </h2>
-                <p className="">
+                <p className="text-zinc-400 mt-3 mb-8 max-w-[50ch] lg:max-w-[30ch]">
                 Reach out today to discuss your project needs and start collaborating on something amazing!   
                 </p>
-                <div className="">
+                <div className="flex items-center gap-2 mt-auto">
                     {socialLinks.map(({ href, icon }, key) => (
                         <a
                          key={key} 
                          href={href}
                          target="_blank"
-                         className=""
+                         className="w-12 h-12 grid place-items-center ring-inset ring-2 ring-zinc-50/5 rounded-lg transition-[background-color,color] hover:bg-zinc-50 hover:text-zinc-950 active:bg-zinc-50/80"
                         >
                             {icon}
                         </a>
@@ -64,12 +64,12 @@ const Contact = () => {
 
             </div>
             <form 
-                action=""
+                action="https://getform.io/f/bwnnjjza"
                 method="POST" 
-                className=""
+                className="xl:pl-10 2xl:pl-20"
             >
-                <div className="">
-                    <div className="">
+                <div className="md:grid md:items-center md:grid-cols-2 md:gap-2">
+                    <div className="mb-4">
                         <label 
                             htmlFor="name" 
                             className="label"
@@ -85,7 +85,49 @@ const Contact = () => {
                             placeholder="Bo Sun"
                             className="text-field" />
                     </div>
+
+                    <div className="mb-4">
+                        <label 
+                            htmlFor="email" 
+                            className="label"
+                        >
+                            Email
+                        </label>
+                        <input 
+                            type="email" 
+                            name="email" 
+                            id="email"
+                            autoComplete="email"
+                            required
+                            placeholder="example@example.com"
+                            className="text-field" />
+                    </div>
                 </div>
+
+                <div className="mb-4">
+                  <label 
+                    htmlFor="message" 
+                    className="label"
+                    >
+                      Message
+                    </label>
+                    <textarea 
+                      name="message" 
+                      id="message"
+                      placeholder="Hi!"
+                      required
+                      className="text-field resize-y min-h-32 max-h-80 [&]:max-w-full w-full justify-center"
+                      >
+
+                      </textarea>
+                </div>
+
+                <button 
+                  type="submit" 
+                  className="btn btn-primary"
+                  >
+                    Submit
+                  </button>
 
             </form>
         </div>
