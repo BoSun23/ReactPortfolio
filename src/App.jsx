a9b754dde599381ff7a0f6329f23ea5a76d9bf92
https://github.com/BoSun23/FSD11-Web-Service.git <sunboadrien@gmail.com>
2025-01-30 11:45:02 -0500

    final

--- src/App.jsx
+++ src/App.jsx
@@ -8,8 +8,13 @@
 import { ReactLenis } from 'lenis/react';
 import gsap from 'gsap';
 import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { useGSAP } from '@gsap/react';
 
-
+/**
+ * Register gsap pluginss
+ * 
+ */
+gsap.registerPlugin(useGSAP, ScrollTrigger);
 /**
  * 
  * Components
@@ -26,6 +31,25 @@ import Footer from "./components/Footer";
 
 
 const App = () => {
+
+    useGSAP(()=>{
+        const elements = gsap.utils.toArray('.reveal-up')
+
+        elements.forEach((element) => {
+            gsap.to(element, {
+                scrollTrigger: {
+                    trigger: element,
+                    start: '-200 bottom',
+                    end: 'bottom 80%',
+                    scrub: true
+                },
+                y:0,
+                opacity: 1,
+                duration: 1,
+                ease: 'power2.out'
+            })
+        });
+    })
     return (
         <ReactLenis root>
         
--- src/components/Review.jsx
+++ src/components/Review.jsx
@@ -4,6 +4,24 @@
  */
 
 /**
+ * Node modules
+ */
+import gsap from 'gsap';
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { useGSAP } from '@gsap/react';
+
+/**
+ * Register gsap pluginss
+ * 
+ */
+gsap.registerPlugin(useGSAP, ScrollTrigger);
+/**
+ * 
+ * Components
+ */
+
+
+/**
  *
  * Components
  */
@@ -50,6 +68,20 @@ const reviews = [
   ];
 
 const Review = () => {
+
+  useGSAP(()=> {
+    gsap.to('.scrub-slide', {
+      scrollTrigger:{
+        trigger: '.scrub-slide',
+        start: '-200% 80%',
+        end: '400% 80%',
+        scrub: true,
+        markers: true
+      },
+      x:-1000
+    })
+  });
+
   return (
     <section
     id="reviews" 
@@ -57,10 +89,10 @@ const Review = () => {
     >
         <dev className="container">
             <h2 
-            className="headline-2 mb-8">
+            className="headline-2 mb-8 reveal-up">
                 What our customers say
             </h2>
-            <div className="flex items-stretch gap-3 w-fit">
+            <div className="scrub-slide flex items-stretch gap-3 w-fit">
                 {reviews.map(({ content, name, imgSrc, company },
                   key)=>
                   (<ReviewCard 
@@ -69,6 +101,7 @@ const Review = () => {
                    imgSrc={imgSrc}
                    company={company}
                    content={content}
+                   className="reveal-up"
                   />))}
             </div>
         </dev>
